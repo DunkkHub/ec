1fca537b5fa46f60a08e1fe36b9af67e6438b03d
ahmedbenabdallah092 <ahmedbenabdallah092@gmail.com>
2025-04-17 15:42:39 +0000

    Add /about and /contact routes

--- server.js
+++ server.js
@@ -3,8 +3,19 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// Home route
 app.get('/', (req, res) => {
   res.send('Hello from Express on Heroku!');
+});
+
+// About route
+app.get('/about', (req, res) => {
+  res.send('This is the About Us page.');
+});
+
+// Contact route
+app.get('/contact', (req, res) => {
+  res.send('This is the Contact Us page.');
 });
 
 app.listen(PORT, () => {
